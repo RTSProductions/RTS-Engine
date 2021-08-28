95377f0eb0c8263301cbaa175b142848f169a637
RTSProductions <reginhurleydavid@MacBook-Air.local>
2021-08-28 14:39:51 -0600

    Fixing window object scale bug

--- RTS-Engine/main.cpp
+++ RTS-Engine/main.cpp
@@ -8,6 +8,10 @@
 #include <GL/glut.h>
 #endif
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 int main(void)
 {
@@ -15,31 +19,57 @@ int main(void)
     
     
 
-    //Initialize the library
+    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    //Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "RTS-Engine", NULL, NULL);
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "RTS-Engine", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    //Make the window's context current
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
+    GLfloat vertices[] =
+    {
+        50, 300, 0.0, /* Top left corner */
+        300, 300, 0.0, /* Top right corner */
+        300, 50, 0.0, /* Bottom left corner */
+        50, 50, 0.0 /* Bottom right corner */
+    };
 
-    //Loop until the user closes the window
+    GLfloat actualVerts[12];
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        // Render here
-        glClear(GL_COLOR_BUFFER_BIT);
+        glGetFloatv(GL_VIEWPORT, actualVerts);
+        printf("==================\n");
+        for(int i = 0; i < 4; ++i)
+        {
+            printf("%d = %f, ", i, actualVerts[i]);
+        }
 
-        // Swap front and back buffers
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        glDrawArrays(GL_QUADS, 0, 4);
+        glDisableClientState(GL_VERTEX_ARRAY);
+        
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        // Poll for and process events
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
